66ca002c5b1c0cf29a25c46ec67ddc13ca4b5924
Dyuvarov <A.Duvarovv@yandex.ru>
2023-09-08 08:37:03 +0300

    extract ip and mac from argv

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "ft_status.h"
 #include "malcolm_validator.h"
 
@@ -13,5 +14,11 @@ int main(int argc, char** argv)
     if (validation_result != SUCCESS) {
         return validation_result;
     }
+
+    char* ip_source = argv[1];
+    char* ip_target = argv[3];
+    char* mac_source = argv[2];
+    char* mac_target = argv[4];
+
     return SUCCESS;
 }
